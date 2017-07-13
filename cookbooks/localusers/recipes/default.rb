0aa7fbf13c1abf758e7f9a9fbab2c6fc91b149b6
root <root@rameshrm4.mylabserver.com>
2017-07-13 03:57:22 +0000

    inclusion of data_bags and environments

---
+++ cookbooks/localusers/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: localusers
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+search(:users, "*:*").each do |data|
+	user data["id"] do
+		comment data["comment"]
+		uid data["uid"]
+		gid data["gid"]
+		home data["home"]
+		shell data["shell"]
+		manage_home true
+	end
+end
